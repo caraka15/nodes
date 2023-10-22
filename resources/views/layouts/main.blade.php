7451fbcfd3dca7a7ea432005ad7068d45153d870
caraka15 <carakawidi07@gmail.com>
2023-10-23 00:15:54 +0700

    add scroll to top

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -48,6 +48,8 @@
     </script>
 </head>
 <body>
+    <button type="button" id="btn-back-to-top"><i class="fa-solid fa-circle-arrow-up fa-2xl"></i></button>
+
   @include('partials.navbar')
 
   <div class="container">
@@ -57,5 +59,32 @@
   @include('partials.footer')
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script>
+    //Get the button
+  let mybutton = document.getElementById("btn-back-to-top");
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+  
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  </script>
 </body>
 </html>
